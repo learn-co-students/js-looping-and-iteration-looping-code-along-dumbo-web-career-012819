09b348bc42ec9ac3fc90ed4fc008b6c0b1e341da
MichaelRomero <liikemike@aim.com>
2019-03-13 15:39:08 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+function printBadges(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    const ele = arr[i]
+    console.log(`Welcome ${ele}! You are employee #${i + 1}.`);
+  }
+
+  return arr;
+}
+
+function tailsNeverFails() {
+  let outcomes = 0
+
+  while (true) {
+    if (Math.random() >= 0.5) {
+      outcomes += 1;
+    } else {
+      return `You got ${outcomes} tails in a row!`;
+    }
+  }
+}
