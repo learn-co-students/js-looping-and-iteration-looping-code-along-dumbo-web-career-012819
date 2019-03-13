7a232bfbc04af89ce9fbb586c12d70421a5aed37
Angelo Spampinato <spampinatodesign@gmail.com>
2019-03-13 09:30:57 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+printBadges(['Ada', 'Brendan', 'Ali']);
+
+function printBadges(employees) {
+  for (let i = 0; i < employees.length; i++) {
+    console.log(`Welcome ${employees[i]}! You are employee #${i + 1}.`);
+  }
+  return employees;
+}
+
+function tailsNeverFails() {
+  let i = 0;
+  while (Math.random() >= 0.5) {
+    i++;
+  }
+  return `You got ${i} tails in a row!`;
+}
