f3950d4afa42d11e8494545c17294438133756f2
Shemar Dacosta <dshemar6@gmail.com>
2019-03-13 14:52:17 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solutions in this file
+
+
+
+const names = ["Ada", "Shemar", "Cris"]
+
+
+const printBadges = params => {
+  let i = 0
+  for (const element of params) {
+    i++
+    console.log("Welcome " + element + "! You are employee #" + i + ".");
+  }
+
+  return params;
+}
+
+printBadges(names)
+
+
+const tailsNeverFails = () => {
+  let tails = 0;
+  while (Math.random() >= 0.5) {
+    tails++;
+  }
+  return `You got ${tails} tails in a row!`;
+}
+
+tailsNeverFails()
