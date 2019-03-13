f28dab05a681325384dd97c09177bb82f4c0b72d
Cristian Leiva <cleiva94.cl@gmail.com>
2019-03-13 15:02:25 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const printBadges = (empName) => {
+  
+  for (let i = 0; i < empName.length; i++) {
+    console.log(`Welcome ${empName[i]}! You are employee #${i + 1}.`)
+  }
+  return empName
+}
+
+const tailsNeverFails = () => {
+  let tails = 0;
+
+  while (Math.random() >= 0.5) {
+    tails++;
+  }
+
+  return `You got ${tails} tails in a row!`;
+}
